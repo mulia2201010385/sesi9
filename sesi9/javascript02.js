cb38033869ad6b4673d5f2d8c28d9beb8197e152
mulia2201010385 <127836583+mulia2201010385@users.noreply.github.com>
2023-05-09 17:03:37 +0800

    Add files via upload
    upload

---
+++ sesi9/javascript02.js
@@ -0,0 +1,2 @@
+document.getElementById("teks1").style.color = "#FF0000"
+document.getElementById("teks2").style.color = "#76b5c5"
---
+++ sesi9/javascript02.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan 2</title>
+</head>
+<body>
+
+    <div id="teks1">
+        <p>Text untuk layer 1</p>
+    </div>
+    <div id=teks2>
+        <p>Text ini untuk layer 2</p>
+    </div>
+
+    <script src="javascript02.js"></script>
+    
+</body>
+</html>
